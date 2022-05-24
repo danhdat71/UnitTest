aeedcd7255392b5fd2471baa5a9f87e32d3835b4
Danh Đạt <datd@bap.jp>
2022-05-24 19:51:54 +0700

    Update : bai 10

--- test/UserTest.php
+++ test/UserTest.php
@@ -3,8 +3,13 @@
 use PHPUnit\Framework\TestCase;
 use App\User;
 
-class Test extends TestCase
+class UserTest extends TestCase
 {
+    public function testEqual()
+    {
+        $this->assertEquals(1, 1);
+    }
+
     public function testReturnFullName()
     {
         $user = new User();
@@ -15,7 +20,7 @@ class Test extends TestCase
         $this->assertNotEquals("Danh Đạt 1", $user->getFullNameVn());
     }
 
-    public function testEmptyByDefault()
+    public function test_empty_string()
     {
         $user = new User();
         $this->assertEmpty($user->getFullNameVn());
